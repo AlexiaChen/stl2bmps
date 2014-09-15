cc60fb7a52fec69052c52e3a7db782976b790f4c
AlexiaChen <suliuran1992@163.com>
2014-09-15 16:32:51 +0800

    modify stl2bmp.h scrpition

--- stl2bmp.h
+++ stl2bmp.h
@@ -1,6 +1,38 @@
 /*******************************************************
-E-mail:brainfvck@foxmail.com
-Author:chenshaohan
+
+ Copyright (C) 2013-2014 by AlexiaChen at Michgen State University
+ All rights reserved 
+ 
+ Redistribution and use in source and binary forms, with or without modification, 
+ are permitted provided that the following conditions are met:
+ 
+ 1. Redistributions of source code must retain the above copyright
+	notice, this list of conditions and the following disclaimer.
+ 
+ 2. Redistributions in binary form must reproduce the above copyright notice, 
+    this list of conditions and the following disclaimer in the documentation and/or other 
+     materials provided with the distribution.
+ 
+ 3. Neither the name of the project nor the names of its contributors may be used to endorse or promote products derived from this software
+    without specific prior written permission.
+		   
+		   
+  THIS SOFTWARE IS PROVIDED BY THE PROJECT AND CONTRIBUTORS ``AS IS'' AND
+  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED.  IN NO EVENT SHALL THE PROJECT OR CONTRIBUTORS BE LIABLE
+  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+  OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+			  
+  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+			     
+		
+@author:AlexiaChen
+
+@brief:This file is an interface of the library.
 
 
 
@@ -11,7 +43,7 @@ Author:chenshaohan
 #ifndef STL2BMP_H
 #define STL2BMP_H
 
-#ifdef stl2bmp_EXPORTS //cbs.dll export function  
+#ifdef stl2bmp_EXPORTS //dll export function  
 #define STL2BMP_API __declspec(dllexport)
 #else
 #define STL2BMP_API
